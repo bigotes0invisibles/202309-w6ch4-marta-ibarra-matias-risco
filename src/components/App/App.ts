6e4aef87c52556fda245565c552e86c18c4ccbcd
Marta Ibarra <martaa.ibarra@hotmail.com>
2023-10-18 23:41:45 +0200

    Add CharacterCard folder & file

--- src/components/App/App.ts
+++ src/components/App/App.ts
@@ -2,8 +2,7 @@ import Component from "../Component/Component";
 
 class App extends Component {
   populate(): void {
-    this.element.innerHTML = `    <ul class="characters-list row list-unstyled"> </ul>
-`;
+    this.element.innerHTML = `<ul class="characters-list row list-unstyled"> </ul>`;
   }
 }
 
