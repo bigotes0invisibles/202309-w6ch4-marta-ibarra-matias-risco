3a550ef4eb93d302a3e05128aa2832eaa8a2c5b8
Matias Risco <matiasriscolagos@gmail.com>
2023-10-19 22:30:17 +0200

    Refactor character parameters

--- src/components/Character/Character.ts
+++ src/components/Character/Character.ts
@@ -2,15 +2,21 @@ import { type CharacterData } from "../../types.js";
 class Character {
   isAlive: boolean;
   series: string;
-  characterData;
+  name;
+  imageSource;
+  age;
+  family;
 
   constructor(characterData: CharacterData) {
-    this.characterData = characterData;
     this.isAlive = true;
+    this.imageSource = characterData.imageSource;
+    this.name = characterData.name;
+    this.age = characterData.age;
+    this.family = characterData.family;
   }
 
   protected communicate() {
-    return `${this.characterData.name} says: `;
+    return `${this.name} says: `;
   }
 
   protected die(): void {
