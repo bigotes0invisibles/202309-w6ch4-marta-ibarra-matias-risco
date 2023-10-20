d7afe2132551c956a3c87aa45d5077d2bb3cdff3
Matias Risco <matiasriscolagos@gmail.com>
2023-10-20 12:03:19 +0200

    Add assertions to all inherits of CharacterData

--- src/types.ts
+++ src/types.ts
@@ -8,17 +8,21 @@ export interface CharacterData {
 
 export interface KingData extends CharacterData {
   yearsOfReign: number;
+  characterType: "King";
 }
 export interface AdvisorData extends CharacterData {
   advises: CharacterData["name"];
+  characterType: "Advisor";
 }
 
 export interface FighterData extends CharacterData {
   weapon: string;
   dexterity: number;
+  characterType: "Fighter";
 }
 
 export interface SquireData extends CharacterData {
   kissAssLevel: number;
   serves: FighterData["name"];
+  characterType: "Squire";
 }
